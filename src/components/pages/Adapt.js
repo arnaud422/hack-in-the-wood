1fb3d2b71a5d2a189a0a7face0df42c59bde4f3e
arnaud422 <48210106+arnaud422@users.noreply.github.com>
2022-06-24 10:11:14 +0100

    Update Adapt.js

--- src/components/pages/Adapt.js
+++ src/components/pages/Adapt.js
@@ -3,12 +3,9 @@ import React, { useEffect } from 'react';
 const Adapt = () => {
     const vibrate = ()=>{
         console.log('tet')
-        window.navigator.vibrate(1000)
+        window.navigator.vibrate([1000,300,1000])
     }
     
-    useEffect(()=>{
-        window.navigator.vibrate([1000,300,1000])
-    })
     return (
         <div>       
             <button onClick={vibrate}>test</button>
