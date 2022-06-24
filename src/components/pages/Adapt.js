614d06b85514c4e94d53b8704a7a835fee896618
arnaud422 <48210106+arnaud422@users.noreply.github.com>
2022-06-24 09:23:38 +0100

    Update Adapt.js

--- src/components/pages/Adapt.js
+++ src/components/pages/Adapt.js
@@ -1,12 +1,13 @@
 import React, { useEffect } from 'react';
 
 const Adapt = () => {
-    useEffect(()=>{
-        window.navigator.vibrate([100,30,100,30,100,30,200,30,200,30,200,30,100,30,100,30,100]); // Vibre 'SOS' en Morse.
-    })
+    const vibrate = ()=>{
+        window.navigator.vibrate(1000)
+    }
+    
     return (
-        <div>
-            Cette est page est la page des personnes avec un soucis de vue
+        <div>       
+            <button onClick={vibrate}>test</button>
         </div>
     );
 };
