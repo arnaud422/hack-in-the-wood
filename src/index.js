1929e484c4ffeb35dd885f5b868bf77d5080c76e
arnaud422 <48210106+arnaud422@users.noreply.github.com>
2022-06-26 00:29:12 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from "react-router-dom";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 // import './index.css';
 import App from './App';
 
