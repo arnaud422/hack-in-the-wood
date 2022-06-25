b6b6ca541d4c014c0c8258fded8b9179c816af9f
arnaud422 <48210106+arnaud422@users.noreply.github.com>
2022-06-26 00:25:43 +0100

    test pwa

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+serviceWorkerRegistration.unregister();
 root.render(
   <React.StrictMode>
     <BrowserRouter>
