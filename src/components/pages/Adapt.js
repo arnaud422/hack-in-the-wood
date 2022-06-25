1fb003a47660e6db086d89b9ddc58709422a8a7f
arnaud422 <48210106+arnaud422@users.noreply.github.com>
2022-06-25 20:39:26 +0100

    Update Adapt.js

--- src/components/pages/Adapt.js
+++ src/components/pages/Adapt.js
@@ -62,7 +62,7 @@ const Adapt = () => {
         if (distanceTrajet >= 15) {
           vibrate(800, 200, 800).then(()=>{console.log('test')})
         } else if (distanceTrajet >= 5) {
-          vibrate(800, 2000, 800).then(()=>{console.log('test')})
+          vibrate(800, 500, 800).then(()=>{console.log('test')})
         } else if (distanceTrajet) {
           vibrate(1500, 2000, 1500).then(()=>{console.log('test')})
         }
