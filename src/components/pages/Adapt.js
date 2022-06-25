8d1a069e28e5ac3d33962ba62ebaee6f7bb98177
arnaud422 <48210106+arnaud422@users.noreply.github.com>
2022-06-25 08:54:37 +0100

    Update Adapt.js

--- src/components/pages/Adapt.js
+++ src/components/pages/Adapt.js
@@ -1,92 +1,128 @@
-import React, { useEffect, useState } from 'react';
-import TransportBtn from '../transportButton';
-import "../style/adapt.css"
-
+import React, { useEffect, useState } from "react";
+import TransportBtn from "../transportButton";
+import "../style/adapt.css";
 
 const Adapt = () => {
+  // const [arrets, setArrets] = useState([])
+  const [arret, setArret] = useState("");
+  const [positionUser, setPositionUser] = useState();
+  const [dist, setDist] = useState();
 
-    // const [arrets, setArrets] = useState([])
-    const [arret, setArret] = useState("")
-    const [dist, setDist] = useState()
+  const arrets = {
+    stops: [
+      { name: "Tente fond", Latitude: 50.601759, Longitude: 3.510466 },
+      { name: "Maker Lab", Latitude: 50.601767, Longitude: 3.510722 },
+      { name: "Point info", Latitude: 50.601324, Longitude: 3.511161 },
+      { name: "Entrée", Latitude: 50.601096, Longitude: 3.511567 },
+    ],
+  };
 
-    const arrets = {
-        "stops":
-            [
-                {"name": "Tente fond", "Latitude": 50.601759, "Longitude": 3.510466},
-                {"name": "Maker Lab", "Latitude": 50.601767, "Longitude": 3.510722},
-                {"name": "Point info", "Latitude": 50.601324, "Longitude": 3.511161},
-                {"name": "Entrée", "Latitude": 50.601096, "Longitude": 3.511567}
-            ]
-        
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(
+        showPosition,
+        (err) => {
+          console.log(err);
+        },
+        { enableHighAccuracy: true, maximumAge: 100, timeout: 500000 }
+      );
+    } else {
+      return false;
     }
 
-    function distance(lat1, lon1, lat2, lon2) {
-        var R = 6378.137; var dLat = lat2 * Math.PI / 180 - lat1 * Math.PI / 180;
-        var dLon = lon2 * Math.PI / 180 - lon1 * Math.PI / 180;
-        var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) * Math.sin(dLon/2) * Math.sin(dLon/2);
-        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); var d = R * c;
-        return Math.floor(d * 1000);
-    }
+    addTecData();
+  }, [addTecData]);
 
-    const vibrate = ()=>{
-        window.navigator.vibrate([700,300,700])
-    }
+  function addTecData() {
+    console.log("test");
+    fetch("https://static.tectime.be/stops?all=true")
+      .then((response) => response.json())
+      .then((data) => triDataStops(data.StopsResult)); //setArrets(data.StopsResult)
+  }
 
-    const handleArrets = (e)=>{
-        setArret(e.target.value)
-    }
-
-      function getLocation() {
-        if (navigator.geolocation) {
-            navigator.geolocation.watchPosition(showPosition, err=>{console.log(err)},  { enableHighAccuracy: true, maximumAge: 100, timeout: 500000 });
-        } else { 
-          return false
-        }
-      }
-
-      function showPosition(position) {
-        const location = arrets.stops.find((spot)=> spot.name === arret)
-
-        if(location === undefined) return
-
-        const long = distance(position.coords.latitude,position.coords.longitude, location.Latitude, location.Longitude)
-        document.getElementById('test').innerText = `${position.coords.latitude} ET ${position.coords.longitude}`
-        
-        if(long <= 7){
-            vibrate()
-        }      
-      }
-    
-    
-    // useEffect(()=>{
-    //     fetch('https://static.tectime.be/stops?all=true')
-    //     .then(response => response.json())
-    //     .then(data => setArrets(data.StopsResult));
-    // },[])
-
-    return (
-        <>
-            <div className='box-transport-method'>       
-                <TransportBtn transport={"bus"} bgColor={"#FFE046"}/>
-                <TransportBtn transport={"train"} bgColor={"#338CF1"}/>
-                <TransportBtn transport={"pied"} bgColor={"#F83340"}/>
-                <TransportBtn transport={"tram"} bgColor={"#30F680"}/>
-            </div>
-
-            <div className="choice-stops">
-                <input list="stops" id='search' onChange={handleArrets}/>
-                <button onClick={getLocation}>commencer</button>
-
-                <datalist id='stops'>
-                    {
-                       arrets.stops.map((arret, i)=> <option value={arret.name} key={i}/>)         
-                    }
-                </datalist>
-                {/* <span className='dist'>{dist}</span> */}
-                <span className='dist' id="test"></span>
-            </div>
-        </>
+  function triDataStops(data) {
+    const stops = data.filter(
+      (stop) =>
+        distance(
+          positionUser.coords.latitude,
+          positionUser.coords.longitude,
+          stop.Latitude,
+          stop.Longitude
+        ) < 30000
     );
+    console.log(stops);
+  }
+
+  function distance(lat1, lon1, lat2, lon2) {
+    var R = 6378.137;
+    var dLat = (lat2 * Math.PI) / 180 - (lat1 * Math.PI) / 180;
+    var dLon = (lon2 * Math.PI) / 180 - (lon1 * Math.PI) / 180;
+    var a =
+      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+      Math.cos((lat1 * Math.PI) / 180) *
+        Math.cos((lat2 * Math.PI) / 180) *
+        Math.sin(dLon / 2) *
+        Math.sin(dLon / 2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    var d = R * c;
+    return Math.floor(d * 1000);
+  }
+
+  const vibrate = () => {
+    window.navigator.vibrate([700, 300, 700]);
+  };
+
+  const handleArrets = (e) => {
+    setArret(e.target.value);
+  };
+
+  function showPosition(position) {
+    const location = arrets.stops.find((spot) => spot.name === arret);
+    setPositionUser(position);
+
+    if (location === undefined) return;
+
+    const long = distance(
+      position.coords.latitude,
+      position.coords.longitude,
+      location.Latitude,
+      location.Longitude
+    );
+
+    if (long <= 7) {
+      vibrate();
+    }
+  }
+
+  // useEffect(()=>{
+  //     fetch('https://static.tectime.be/stops?all=true')
+  //     .then(response => response.json())
+  //     .then(data => setArrets(data.StopsResult));
+  // },[])
+
+  return (
+    <>
+      <div className="box-transport-method">
+        <TransportBtn transport={"bus"} bgColor={"#FFE046"} />
+        <TransportBtn transport={"train"} bgColor={"#338CF1"} />
+        <TransportBtn transport={"pied"} bgColor={"#F83340"} />
+        <TransportBtn transport={"tram"} bgColor={"#30F680"} />
+      </div>
+
+      <div className="choice-stops">
+        <input list="stops" id="search" onChange={handleArrets} />
+        <button>commencer</button>
+
+        <datalist id="stops">
+          {arrets.stops.map((arret, i) => (
+            <option value={arret.name} key={i} />
+          ))}
+        </datalist>
+        {/* <span className='dist'>{dist}</span> */}
+        <span className="dist" id="test"></span>
+      </div>
+    </>
+  );
 };
 
 export default Adapt;
